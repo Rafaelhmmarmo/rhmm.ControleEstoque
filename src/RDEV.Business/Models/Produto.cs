9bd808c5a101160ea391e6f4d29a7e1d024267f9
Rafael H <rafaelhmmarmo@gmail.com>
2021-12-15 19:34:48 -0300

    Setup da aplicação completa

---
+++ src/RDEV.Business/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDEV.Business.Models
+{
+    public class Produto
+    {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public string Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
